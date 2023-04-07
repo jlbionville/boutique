44acd64802bfb77b3e088d044072f9f4eab546f5
jlb <jlbionville@alfaco.fr>
2023-04-07 15:12:24 +0200

    composant achat adapté au nouveau format de donnée

--- src/components/PurchaseList.vue
+++ src/components/PurchaseList.vue
@@ -1,12 +1,31 @@
 <template>
-    <div>
-      <h1>Purchase List</h1>
-      <ul>
-        <li v-for="purchase in purchases" :key="purchase.id">
-          {{ purchase.name }}
-        </li>
-      </ul>
-    </div>
+<div class="container">
+		<h1>My Purchases</h1>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Item Name</th>
+					<th>Description</th>
+					<th>Tags</th>
+					<th>Prices</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="item in purchases">
+					<td>{{ item.libelle }}</td>
+					<td>{{ item.description }}</td>
+					<td>{{ item.tags.join(', ') }}</td>
+					<td>
+						<ul class="list-unstyled">
+							<li v-for="price in item.prix">
+								{{ price.magasin }}: {{ price.prix }} ({{ price.modele }})
+							</li>
+						</ul>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
   </template>
   
   <script>
