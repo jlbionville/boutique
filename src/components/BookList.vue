edf8b61486e26d5f11542703ac084f360af89c9b
jlb <jlbionville@alfaco.fr>
2023-04-07 15:04:17 +0200

    ajout de la vue et de la liste pour la liste des livres

---
+++ src/components/BookList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>Book List</h1>
+      <ul>
+        <li v-for="book in books" :key="book.title">
+          {{ book.title }} | {{ book.auteur }} |{{ book.image }} 
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import booksData from '@/data/books.json';
+  
+  export default {
+    data() {
+      return {
+        books: booksData
+      }
+    }
+  }
+  </script>
+  
---
+++ src/views/BookListView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <book-list></book-list>
+      
+    </div>
+  </template>
+  
+  <script>
+  import bookList from '@/components/BookList.vue';
+  
+  export default {
+    components: {
+      bookList
+    }
+  }
+  </script>
+  
