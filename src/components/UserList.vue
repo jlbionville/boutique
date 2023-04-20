3650a625c3d1124add88a98572cb0a594eb89846
jlb <jlbionville@alfaco.fr>
2023-04-20 17:09:04 +0200

    affichage dans un tableau

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,17 +1,32 @@
 <template>
-    <div>
-      <h1>User List</h1>
-      <ul>
-        <li v-for="user in users" :key="user.id">
-          {{ user.name }}
-        </li>
-      </ul>
-    </div>
-  </template>
-  
+  <div class="container">
+      <thead>
+        <tr>
+          
+          <th>
+            Name
+          </th>
+          <th>
+            surname
+          </th>
+          <th>
+            email
+          </th>          
+        </tr>
+      </thead>
+      <tbody >
+        <tr v-for="user in users" :key="user.id">
+          <td>{{ user.name }}</td>
+          <td>{{ user.surname }}</td>
+          <td>{{ user.email }}</td>
+        </tr>
+      </tbody>
+    
+  </div>
+</template>
+
   <script>
-  import usersData from '@/data/users.json';
-  
+  import usersData from '@/data/users.json'
   export default {
     data() {
       return {
